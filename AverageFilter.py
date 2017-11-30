fa716f86b0aaf81259e6c0a2cdb2bc01d5438a80
Mahir <mahir1010@gmail.com>
2017-11-30 11:02:08 +0530

    Added a few comments to explain what is going on

--- AverageFilter.py
+++ AverageFilter.py
@@ -5,16 +5,29 @@ import time
 
 
 kernel_width=3
-
+"""	
+	avg_filter : A function which uses a window to traverse over every pixels of the image .
+	It computes average of every elements in a window and and sets every element to that value.
+	This function runs parallely by computing window at every pixel simultaneously.
+	Number of Thread Blocks = Number of Pixels
+	Number of Threads = Number of Thread Blocks * power(kernel_size,2)
+	Parameters:
+	pixels: An array of size mxn pixels where m and n are the dimensions of an image ane the elements of the array are the intensity 
+	values at every pixel.
+"""
 @cuda.jit('void(uint8[:,:],int32,int32)')
 def avg_filter(pixels,m,n):
-	sub_img=cuda.shared.array(shape=(kernel_width,kernel_width),dtype=uint8)
+	sub_img=cuda.shared.array(shape=(kernel_width,kernel_width),dtype=uint8) #A shared array to store the elements of the kernel
 	block_id_x=cuda.blockIdx.x
 	block_id_y=cuda.blockIdx.y
 	thread_id_x=cuda.threadIdx.x
 	thread_id_y=cuda.threadIdx.y
 	Memory_position_x=block_id_x+thread_id_x
-	Memory_position_y=block_id_y+thread_id_y	
+	Memory_position_y=block_id_y+thread_id_y
+	"""
+		isVisible computes whether the pixel actually exists or not
+		is false for every pixels outside the image boundary hence the intensity level must be set to 0
+	"""	
 	isVisible= False if Memory_position_x < 0 or Memory_position_x > m or Memory_position_y < 0 or Memory_position_y > n else True
 	if not isVisible:
 		sub_img[thread_id_x,thread_id_y]=0
